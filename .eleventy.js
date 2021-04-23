28068f64aee6caecb6821ea1be242d067f08ee92
matkat99 <st83440@gmail.com>
2021-04-22 19:43:12 -0600

    remove sass plugin

--- .eleventy.js
+++ .eleventy.js
@@ -5,14 +5,14 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-const pluginSass = require("eleventy-plugin-sass");
+// const pluginSass = require("eleventy-plugin-sass");
 module.exports = function (eleventyConfig) {
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
 
-  eleventyConfig.addPlugin(pluginSass, { outputDir: "" });
+  // eleventyConfig.addPlugin(pluginSass, { outputDir: "" });
 
   // https://www.11ty.dev/docs/data-deep-merge/
   eleventyConfig.setDataDeepMerge(true);
